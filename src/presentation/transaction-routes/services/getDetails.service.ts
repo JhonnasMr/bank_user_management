0eb9e417b276bbab36c576c9d4f8cd808f7c9ac9
@JhonnasDEV <97009831+JhonnasMr@users.noreply.github.com>
2025-04-07 02:19:44 -0500

    next commit

--- src/presentation/transaction-routes/services/getDetails.service.ts
+++ src/presentation/transaction-routes/services/getDetails.service.ts
@@ -5,24 +5,18 @@ export class GetDetailsService {
 
     async execute(transactionID: string) {
 
-        try {
+        const transaction = await TransactionModel.findOne({
+            where: {
+                id: transactionID
+            },
+            relations: ['user']
+        })
 
-            const transaction = await TransactionModel.findOne({
-                where: {
-                    id: transactionID
-                },
-                relations: ['user']
-            })
-
-            if (!transaction) {
-                throw CustomError.notFound('transaction not found');
-            }
-
-            return transaction;
-
-        } catch (error) {
-            throw CustomError.internalServer('something went wrong 💥');
+        if (!transaction) {
+            throw CustomError.notFound('transaction not found');
         }
+
+        return transaction;
 
     }
 
--- src/presentation/transaction-routes/services/rechargeBalance.service.ts
+++ src/presentation/transaction-routes/services/rechargeBalance.service.ts
@@ -4,31 +4,26 @@ import { CustomError } from "../../../domain";
 export class RechargeBalanceService {
 
     async execute(account_number: number, amount: number) {
-        try {
 
-            const user = await UserModel.findOne({
-                where: {
-                    account_number: account_number,
-                    email_verified: true
-                }
-            })
-
-            if (!user) {
-                throw CustomError.notFound('User not found')
+        const user = await UserModel.findOne({
+            where: {
+                account_number: account_number,
+                email_verified: true
             }
+        })
 
-            user.balance = Number(user.balance) + amount;
-            await user.save();
-
-            return {
-                message: 'Balance recharged successfully',
-                account_number,
-                amount
-            };
-
-        } catch (error) {
-            CustomError.internalServer('recharge not completed 😒');
+        if (!user) {
+            throw CustomError.notFound('User not found')
         }
+
+        user.balance = Number(user.balance) + amount;
+        await user.save();
+
+        return {
+            message: 'Balance recharged successfully',
+            account_number,
+            amount
+        };
 
     }
 
